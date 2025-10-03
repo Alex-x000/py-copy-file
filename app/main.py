76665488664a7d2960b3ff20461adf536a2e9638
Sasha <alex.acmate@gmail.com>
2025-10-03 16:34:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:  # copy_file("cp file.txt file.txt")
+    if (len(command.split(" ")) != 3 or command.split(" ")[0] != "cp"
+            or command.split(" ")[1] == command.split(" ")[2]):
+        return
+    else:
+        try:
+            _, file, new_file = command.split(" ")
+            with open(file, "r") as f1, open(new_file, "w") as f2:
+                for line in f1.readline():
+                    f2.write(line)
+        except FileNotFoundError:
+            pass
