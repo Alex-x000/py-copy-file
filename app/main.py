3c751b4185b20848eee4e2b347f2077c8c1a8a77
Sasha <alex.acmate@gmail.com>
2025-10-05 01:44:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 def copy_file(command: str) -> None:  # copy_file("cp file.txt file.txt")
-    parts = command.split(" ")
-    if (len(parts) != 3 or parts[0] != "cp"
-            or parts[1] == parts[2]):
+    spl = command.split(" ")
+    if (len(spl) != 3 or spl[0] != "cp"
+            or spl[1] == spl[2]):
         return
     else:
         try:
-            with open(parts[1], "r") as f1, open(parts[2], "w") as f2:
-                for line in f1.readline():
+            _, file, new_file = spl
+            with open(file, "r") as f1, open(new_file, "w") as f2:
+                for line in f1.read():
                     f2.write(line)
         except FileNotFoundError:
             pass
