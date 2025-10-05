63ece6c50aae32d4d1cd8e5e29e72cfa536716da
Sasha <alex.acmate@gmail.com>
2025-10-05 22:29:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 def copy_file(command: str) -> None:
-    spl = command.split(" ")
-    if (len(spl) != 3 or spl[0] != "cp"
-            or spl[1] == spl[2]):
+    if (len(command.split(" ")) != 3 or command.split(" ")[0] != "cp"
+            or command.split(" ")[1] == command.split(" ")[2]):
         return
     else:
         try:
-            _, file, new_file = spl
+            cp, file, new_file = command.split(" ")
             with open(file, "r") as f1, open(new_file, "w") as f2:
                 for line in f1.read():
                     f2.write(line)
